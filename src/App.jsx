891649feea17609127d5c0198ea9c51e8d7b9dc9
threeanra <trian.radis448@gmail.com>
2024-10-17 10:12:05 +0700

    Rename url in route notification

--- src/App.jsx
+++ src/App.jsx
@@ -339,7 +339,7 @@ function App() {
             }
           />
           <Route
-            path="/notification"
+            path="/notifications"
             element={
               <PrivateRoute>
                 <NotificationPage />
